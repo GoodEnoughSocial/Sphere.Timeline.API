e86f8ba185d317c80d8d0504f2f5eb0c3151ede4
Chad Jessup <chad.jessup@gmail.com>
2022-06-26 00:59:41 -0700

    hooking up to Orleans

--- Sphere.Timeline.API/Program.cs
+++ Sphere.Timeline.API/Program.cs
@@ -10,6 +10,7 @@ try
 {
     var builder = WebApplication.CreateBuilder(args);
     builder.Host.UseSerilog(SphericalLogger.SetupLogger);
+    builder.Services.AddInjectableOrleansClient();
     builder.Services.AddHealthChecks();
 
     // Add services to the container.
